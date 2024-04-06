c45ecd01a8a9b2da121fb0e1d07648776e722479
Garrett Gee <garrett.m.gee@gmail.com>
2024-04-06 10:14:08 -0400

    almost done

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
 const endpoint = "https://todo.hackrpi.com";
 const addListElement = document.getElementById("add-list");
+const listContainerElement = document.getElementById('list-container');
+const newListInputElement = document.getElementById('new-list-input');
 const API_KEY = "2be3095525d52048f21cc456f6b4b584";
 
+//Get status with /status GET endpoint
+async function getStatus() {
+    try {
+        const response = await fetch(endpoint+'/status', {
+            method: 'GET',
+            headers: {
+                'authorization': API_KEY,
+                'Content-Type': 'application/json'
+            }
+        })
+        const status = await response.json();
+        document.getElementById("status").innerText = status.message;
+    } catch(e){
+        console.error('Error getting status:' + e);
+    }
+}
+
+
+//Event listeners for menu
 addListElement.addEventListener("click", function(){
     addList();
 });
 
-const listContainerElement = document.getElementById('list-container');
-const newListInputElement = document.getElementById('new-list-input');
+newListInputElement.onchange = function(){
+    addList();
+};
 
-//Get all items from server
+
+//Get all items from server and run the render function
 async function fetchLists() {
     try {
-        const response = await fetch(endpoint+'/GetLists', {
-            headers:{
-                'authorization':API_KEY,
-                'Content-Type': 'application/json',
+        let lists = [];
+        const getListResponse = await loopRequest("");
+        async function loopRequest(newToken){
+            const response = await fetch(`${endpoint}/GetLists/` + (newToken !== "" ? "?"+new URLSearchParams({
+                nextToken: newToken
+            }) :""), {
+                method: 'GET',
+                headers: {
+                    'authorization':API_KEY,
+                    'Content-Type': 'application/json',
+                }
+            });
+            const newLists = await response.json();
+            console.log("new LISTS to add: "+ JSON.stringify(newLists));
+            if(newLists.status == "200"){
+                lists = lists.concat(newLists.lists);
             }
-        });
-        const lists = await response.json();
-        console.log(lists);
-        console.log(JSON.stringify(lists));
-        await renderLists(lists.lists);
+            console.log("LISTS iteration: " + JSON.stringify(lists));
+
+            if (newLists.nextToken && newLists.nextToken !== "NULL" ){
+                return loopRequest(newLists.nextToken);
+            } else {
+                console.log(newLists.nextToken);
+                return lists;
+            }
+        };
+        await renderLists(getListResponse);
     } catch (error) {
         console.error('Error fetching lists:', error);
     }
 }
 
-//Modifies DOM and sends post request
+//Adds list through /AddList POST endpoint
 async function addList() {
     const title = newListInputElement.value.trim();
     if (title) {
@@ -43,7 +83,17 @@ async function addList() {
                 })
             });
             const newList = await response.json();
-            renderList(newList);
+            //Resync lists since its easier to do that
+            if(newList.status == "200"){
+                console.log(JSON.stringify(newList));
+                renderList({
+                    id: newList.list.listId,
+                    listName: newList.list.listName,
+                    items: []
+                });
+            }
+            
+
             newListInputElement.value = '';
         } catch (error) {
             console.error('Error adding list:', error);
@@ -51,7 +101,7 @@ async function addList() {
     }
 }
 
-//Modifies DOM and sends delete request
+//Deletes list through /DeleteList DELETE endpoint
 async function deleteList(listIdParam) {
     try {
     await fetch(`${endpoint}/DeleteList?` + new URLSearchParams({
@@ -71,28 +121,28 @@ async function deleteList(listIdParam) {
 }
 
 
-//Only renders each list
+//Renders each list given an array of list objects
 async function renderLists(lists) {
     console.log("render lists: "+JSON.stringify(lists));
-    listContainerElement.innerHTML = '';
-    lists.forEach(async function(e){
-        //request items from specific list
-        //render list
-        console.log("element id: "+e.id);
-        const listItems = await getListItems(e.id);
-        console.log(listItems);
+    //To preserve the sequence of lists, use for loop instead of forEach (which would run functions in parallel)
+    let listItems;
+    for (const e of lists){
+        listItems = await getListItems(e.id);
         renderList({
             id: e.id,
             listName: e.listName,
             items: listItems
         });
-    });
+    }
+    let loadingEl=document.getElementById('loading');
+    if(loadingEl!==null) loadingEl.remove();
+    
 }
 
+//Get all list items using GetListItems GET endpoint until next token is exhausted
 async function getListItems(listIdParam){
     try {
         let listItems = [];
-
         const getListResponse = await loopRequest("");
 
         async function loopRequest(newToken){
@@ -111,22 +161,22 @@ async function getListItems(listIdParam){
             console.log("getting list "+ listIdParam);
             const newItems = await response.json();
             console.log("new items to add: "+ JSON.stringify(newItems));
-            listItems = listItems.concat(newItems.listItems);
+            if(newItems.status == "200"){
+                listItems = listItems.concat(newItems.listItems);
+            }
             console.log("list item iteration: " + JSON.stringify(listItems));
 
-            if (newItems.tempToken && newItems.tempToken !== "NULL" ){
-                return loopRequest(newItems.tempToken);
+            if (newItems.nextToken && newItems.nextToken !== "NULL" ){
+                return loopRequest(newItems.nextToken);
             } else {
-                console.log(newItems.tempToken);
+                console.log("token:" + newItems.nextToken);
                 return listItems;
             }
         }
 
-        console.log("list items returned: " + getListResponse);
+        console.log("list items returned: " + JSON.stringify(getListResponse));
         
         return getListResponse;
-        // const listElement = document.getElementById(`list-${listId}`);
-        // const taskList = listElement.querySelector('ul');
     } catch (error) {
         console.error('Error getting list items:', error);
     }
@@ -149,18 +199,21 @@ function renderList(list) {
     let tempHTML = `
     <div id="list-${list.id}" class="list">
         <h2 class="list-header">${list.listName}</h2>
-        <input id="task-input-${list.id}" type="text">
+        <input id="task-input-${list.id}" type="text" value="" class="text-input">
         <button id="add-items-${list.id}">Add</button>
         <button id="delete-list-${list.id}">Delete List</button>
         <div class="item-list"></div>
     </div>
     `;
 
+    let loadingEl=document.getElementById('loading');
+    if(loadingEl!==null) loadingEl.remove();
+
     document.getElementById("list-container").insertAdjacentHTML("beforeend", tempHTML);
     document.getElementById(`delete-list-${list.id}`).onclick = () => deleteList(list.id);
     document.getElementById(`add-items-${list.id}`).onclick = () => addTask(list.id);
-
-    console.log("current list: " + JSON.stringify(list));
+    document.getElementById(`task-input-${list.id}`).onchange = () => addTask(list.id);
+    
     list.items.forEach(task => {
         console.log("task: ");
         console.log(task);
@@ -169,30 +222,34 @@ function renderList(list) {
 
 }
 
-//Only renders
+//Renders each to-do task
 function createTaskElement(task, listId) {
+    console.log("rendering current task: " + JSON.stringify(task));
     let tempHTML = `
-    <div id="task-${task.id}" class="item ${task.checked ? "completed":""}">
-        <input id="input-${task.id}" "type="text" value=${task.itemName}>
-        <button id="delete-${task.id}">Delete Item</button>
-        <label>
+    <div id="task-${task.id}" class="item${task.checked ? " completed":""}">
+        <label class="checkbox-label">
             <input id="checkbox-${task.id}" type="checkbox" ${task.checked ? "checked":""}>
+            <div class="checkbox-display"></div>
         </label>
+        <input id="input-${task.id}" "type="text" value="${task.itemName}" class="text-input task-input">
+        <button id="delete-${task.id}">Delete Item</button>
     </div>
     `;
 
-    document.getElementById("list-"+listId).querySelector(".item-list").insertAdjacentHTML("beforeend", tempHTML);
-    document.getElementById(`checkbox-${task.id}`).onchange = (e) => {
+    document.getElementById("list-"+listId).querySelector(".item-list").insertAdjacentHTML("beforebegin", tempHTML);
+    document.getElementById(`input-${task.id}`).onchange = (e) => {
         renameTask(listId, task.id, document.getElementById(`input-${task.id}`).value);
     };
-    document.getElementById(`checkbox-${task.id}`).onchange = function(){
-        console.log("this value: "+this.value === "on" ? true : false);
-        setCheckedTask(listId, task.id, this.value === "on" ? true : false);
+    document.getElementById(`checkbox-${task.id}`).onchange = function(e){
+        console.log("this value: "+e.target.checked);
+        document.getElementById(`task-${task.id}`).classList.toggle('completed', e.target.checked);
+        setCheckedTask(listId, task.id, e.target.checked);
     };
     document.getElementById(`delete-${task.id}`).onclick = () => deleteTask(listId, task.id);
 
 }
 
+//Adds task through /AddListItem post endpoint
 async function addTask(listIdParam) {
     const taskInput = document.getElementById(`task-input-${listIdParam}`);
     const description = taskInput.value.trim();
@@ -211,8 +268,6 @@ async function addTask(listIdParam) {
                 })
             });
             const newTask = await response.json();
-            // const listElement = document.getElementById(`list-${listId}`);
-            // const taskList = listElement.querySelector('ul');
             createTaskElement(newTask.listItem, listIdParam);
             taskInput.value = '';
         } catch (error) {
@@ -221,47 +276,48 @@ async function addTask(listIdParam) {
     }
 }
 
+//Rename task through /RenameItem/ PATCH endpoint
 async function renameTask(listId, thisItemId, newName) {
     try {
-        await fetch(`${endpoint}/RenameItem`, {
+        console.log(thisItemId);
+        console.log(newName);
+        await fetch(`${endpoint}/RenameItem/?${new URLSearchParams({
+            itemId: thisItemId,
+            newItemName: newName
+        })}`, {
             method: 'PATCH',
             headers: {
                 'authorization':API_KEY,
                 'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                itemId: thisItemId,
-                newItemName: newName
-            })
+            }
         });
-        const taskElement = document.getElementById(`task-${taskId}`);
-        taskElement.classList.toggle('completed', completed);
     } catch (error) {
         console.error('Error updating task:', error);
     }
 }
 
+//Set checked task through /SetChecked/ PATCH endpoint
 async function setCheckedTask(listId, thisItemId, newChecked) {
     try {
+        console.log(thisItemId);
         console.log(newChecked);
-        await fetch(`${endpoint}/SetChecked`, {
+        
+        const response = await fetch(`${endpoint}/SetChecked/?${new URLSearchParams({
+            itemId: thisItemId,
+            checked: newChecked
+        })}`, {
             method: 'PATCH',
             headers:{
                 'authorization':API_KEY,
                 'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                itemId: thisItemId,
-                checked: newChecked
-            })
+            }
         });
-        const taskElement = document.getElementById(`task-${thisItemId}`);
-        taskElement.classList.toggle('completed', newChecked);
     } catch (error) {
         console.error('Error updating task:', error);
     }
 }
 
+//Deletes task through /DeleteListItem/ DELETE endpoint
 async function deleteTask(listIdParam, taskId) {
     try {
         await fetch(`${endpoint}/DeleteListItem/?${new URLSearchParams({
@@ -281,3 +337,4 @@ async function deleteTask(listIdParam, taskId) {
 }
 
 fetchLists();
+getStatus();
